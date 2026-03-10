ed663f41d0293f3a832149c27dc6ac6daf2a39c2
hei hei OK OK! <98307235+laisammy@users.noreply.github.com>
2026-03-10 01:08:32 +0000

    Re-imported images

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,3 +1,6 @@
+import riceThumb from "./assets/06022026_thumbnail.png";
+import knightThumb from "./assets/Screenshot 2026-02-26 203638.png";
+
 export default function Projects () {
     return (
         <div className = "flex-col items-baseline gap-3 bg-violet-300 p-4 rounded-lg">
@@ -7,10 +10,10 @@ export default function Projects () {
             <p> Here are some of the projects I've done: </p>
             <div className="flex-col">
                 <a href="https://www.roblox.com/games/82479517875218/This-This-Rice" className="font-bold underline hover:bg-purple-200"> This This Rice </a>
-                <img src="src/assets/06022026_thumbnail.png" className="max-w-2xl max-h-2xl m-5" alt="This This Rice Thumbnail"/>
+                <img src={riceThumb} className="max-w-2xl max-h-2xl m-5" alt="This This Rice Thumbnail"/>
 
                 <a href="https://laisammy.itch.io/knight-platformer-game" className="font-bold underline hover:bg-purple-200"> Knight Platformer Game </a>
-                <img src="src/assets/Screenshot 2026-02-26 203638.png" className="max-w-2xl max-h-2xl m-5" alt="Knight Platformer Game Thumbnail"/>
+                <img src={knightThumb} className="max-w-2xl max-h-2xl m-5" alt="Knight Platformer Game Thumbnail"/>
             </div>
         </div>
     );
